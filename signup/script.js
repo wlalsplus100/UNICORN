71c8f0d2deae7648b502f352ae8563651ee292ae
wlals <jiminelp@dsm.hs.kr>
2023-06-27 21:51:07 +0900

    📝💄✨:: 광고추가, footer추가, signup ID확인 텍스트 추가

--- signup/script.js
+++ signup/script.js
@@ -1,13 +1,23 @@
 const password = document.querySelector(".passwordinput");
+const idDup = document.querySelector(".DuplicateID");
 const passwordCheck = document.querySelector(".passwordcheckinput");
 const passwordText = document.querySelector(".passwordDiff");
+const idDupText = document.querySelector(".idDup");
+const idInput = document.querySelector(".idinput");
 
-// password.addEventListener("input", CheckPasswordMatch);
+idDup.addEventListener("click", () => {
+  idDupText.style.display = "inline-block";
+});
+
+idInput.addEventListener("input", () => {
+  idDupText.style.display = "none";
+});
+
 passwordCheck.addEventListener("input", CheckPasswordMatch);
 
 function CheckPasswordMatch() {
   if (password.value !== passwordCheck.value) {
-    passwordText.style.display = "inline";
+    passwordText.style.display = "inline-block";
   } else {
     passwordText.style.display = "none";
   }
